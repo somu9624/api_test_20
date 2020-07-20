93f159ae2dcab17a2290e848553fd94424eb0c61
Somasundaram <somu9624@gmail.com>
2020-07-21 01:36:32 +0530

    Third Commit

--- api_gettest.py
+++ api_gettest.py
@@ -1,5 +1,19 @@
 import requests
+import jsonpath
 
-details = "https://reqres.in/api/users/2"
-response = requests.get (details)
-print(response.text)
+def get():
+    details = "https://reqres.in/api/users/2"
+    response = requests.get (details)
+    assert response.status_code == 200
+    if response.status_code == 200:
+        print("Success")
+    else:
+        print("Failure")
+    print(response.text)
+
+    abc = jsonpath.jsonpath(response.json(),'data.id')
+    if abc[0] == 2:
+        print ("Assertion Passed")
+    else:
+        print("Assertion Failed")
+get()
